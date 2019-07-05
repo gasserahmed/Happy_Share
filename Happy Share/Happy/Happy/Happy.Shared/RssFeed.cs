699799264721575a34c4be070cb2b5e34b927925
GASSER\Gasser <gassermontaser@gmail.com>
2019-07-05 17:13:07 -0400

    Fixed quotes error and updated the video link

--- Happy Share/Happy/Happy/Happy.Shared/RssFeed.cs
+++ Happy Share/Happy/Happy/Happy.Shared/RssFeed.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Web.Syndication;
@@ -10,21 +11,21 @@ namespace Happy
     {
         public async static Task< List<QuoteRss> > addItem()
         {
-            List<QuoteRss> quoteList=new List<QuoteRss>();
-            SyndicationClient Client = new SyndicationClient();
-            Uri RSSUri = new Uri("http://happyquotes.tumblr.com/rss");
-            var feeds = await Client.RetrieveFeedAsync(RSSUri);
-            foreach (var feed in feeds.Items)
+            List<QuoteRss> quoteList = new List<QuoteRss>();
+            try
             {
-                if (feed.Title.Text.StartsWith("\"") & !feed.Title.Text.Contains("..."))
-                    quoteList.Add(new QuoteRss { quoteText = feed.Title.Text.Substring(0, feed.Title.Text.Length - 2)+"\"" });
+                SyndicationClient Client = new SyndicationClient();
+                Uri RSSUri = new Uri("http://happyquotes.tumblr.com/rss");
+                var feeds = await Client.RetrieveFeedAsync(RSSUri);
+                foreach (var feed in feeds.Items)
+                {
+                    if (feed.Title.Text.StartsWith("\"") & !feed.Title.Text.Contains("..."))
+                        quoteList.Add(new QuoteRss { quoteText = feed.Title.Text.Substring(0, feed.Title.Text.Length - 2) + "\"" });
+                }
             }
-           Uri RSSUri1 = new Uri("http://www.inspirationline.com/inspirationline.xml");
-            var feeds1 = await Client.RetrieveFeedAsync(RSSUri1);
-            foreach (var feed in feeds1.Items)
+            catch (Exception exception) 
             {
-                 if (!feed.Summary.Text.StartsWith("&"))
-                    quoteList.Add(new QuoteRss { quoteText = "\"" + feed.Summary.Text.Substring(0, feed.Summary.Text.Length - 3) + "\"" });
+                Debug.WriteLine("Error while getting quotes: "+exception);
             }
             return quoteList;
         }
